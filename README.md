a974160d3441fc284acade6fd2ae4e18dcde34af
Maxime <ghalem.maxime@gmail.com>
2020-12-27 15:55:56 +0100

    add fichier applicationContext.xml plus implementation context dans le main

--- README.md
+++ README.md
@@ -1,9 +1,49 @@
 ## Projet Scratch
-Le but de ce projet est de permettre la compréhension des mécanismes d'inversion de contrôle.
-Pour facilité la tâche, les classes qui y sont contenus, sont volontairement succinct.
+Le but de ce projet est de permettre la compréhension des mécanismes d'inversion de contrôle et
+l'injection des dépendance en java avec le Framework Spring.
 
-L'utilisation d'un fichier n'est pas la meilleure solution.
-Ce projet n'a pas pour but de montrer exactement comme Spring par exemple fonctionne,
-mais de démontre le mécanisme d'inversion de contrôle permettent le découplage entre les objets
-l'application.
+Pour mettre en place le conteneur leger de spring (Spring Lightweight container), un fichier de 
+configuration de type "xml" va permettre la création des instances et d'injecter les dépendances 
+au démarrage de l'application.
+
+On appelle ce mécanisme l'inversion de contrôle. Dans le repertoire resources, vous allez trouver 
+le fichier "applicationContext.xml" qui permet de créer au démarrage de l'application la création 
+puis l'injection des composant.
+
+Pour utilisé cette configuration il faut importer la dépendance suivant :
+
+```xml
+<dependency>
+      <groupId>org.springframework</groupId>
+      <artifactId>spring-context</artifactId>
+      <version>5.2.8.RELEASE</version>
+</dependency>
+```
+
+Cette bibliothéque via le fichier applicationContext.xml, permet de charger les classes ciblés 
+puis de gardés en mémoire tout les composant instancier pendant la durée de l'application.
+Ces composants sont gardé sous la forme de graphe (structure de données) pour matérialisé les 
+dépendances entre les composants
+
+#### Exemple de configuration :
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="http://www.springframework.org/schema/beans
+    http://www.springframework.org/schema/beans/spring-beans.xsd">
+
+
+    <bean id="ID_servicePrefix" class="com.project.myProduit.service.ClientServiceDefault">
+        <property name="clientData" ref="ID_postgres"/>
+    </bean>
+
+    <bean id="ID_console" class="com.project.myProduit.controller.IfConsole">
+        <property name="clientService" ref="ID_servicePrefix"/>
+    </bean>
+    
+    <bean id="ID_postgres" class="com.project.myProduit.dao.ClientDataPostgres"/>
+
+</beans>
+```
 
--- src/main/java/com/project/myProduit/App.java
+++ src/main/java/com/project/myProduit/App.java
@@ -9,6 +9,10 @@ import com.project.myProduit.dao.IClientData;
 import com.project.myProduit.service.ClientServiceDefault;
 import com.project.myProduit.service.ClientServicePrefix;
 import com.project.myProduit.service.IClientService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import sun.security.ec.point.ProjectivePoint;
 
 import java.io.File;
@@ -20,61 +24,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
 /*
- * Ce Projet de type scratch donne un aprecut aperçut de la gestion des
- * dépendance faiblement coupler.
- *
+ * Projet scratch
  */
 public class App {
 
-    /* Au lancement de l'application, un choix est fait par l'utilisateur
-     * ce choix permet de fair une sélection d'implementation.
-     */
-
     public static void main(String[] args) {
-        Scanner scName = null;
-        List<String> listClass = new ArrayList<>();
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-            try {
-                String name ;
-                File file  = new File(String.valueOf(Paths.get("resources/config.txt")));
-                scName = new Scanner(file);
-
-                while (scName.hasNext()){
-                    name = scName.next();
-
-                    listClass.add( name);
-                }
-
-            } catch (IOException io) {
-                System.out.println("Error IO exception" + io.getMessage() + io.getCause().toString());
-
-            } catch (Exception ex) {
-                System.out.println("Error Exception Scanner classe :" + ex.getMessage() + ex.getCause().toString());
-            }
-
-        IController iController = null;
-        IClientService iClientService = null;
-        IClientData iClientData = null;
-        try {
-            iController = (IController) Class.forName(listClass.get(0)).
-                    getDeclaredConstructor().
-                    newInstance();
-            iClientService = (IClientService) Class.forName(listClass.get(1)).
-                    getDeclaredConstructor().
-                    newInstance();
-            iClientData = (IClientData) Class.forName(listClass.get(2)).
-                    getDeclaredConstructor().
-                    newInstance();
-        } catch (ClassNotFoundException clex) {
-            System.out.println(clex.getException() + clex.getMessage() + clex.getCause().toString());
-
-        } catch (Exception ex) {
-            System.out.println("Error Exception IOC :" + ex.getMessage() + ex.getCause().toString());
-        }
-
-        iController.setClientService(iClientService);
-        iClientService.setClientData(iClientData);
+        IController iController = context.getBean(IController.class);
 
         iController.createInterfaceControl();
 
